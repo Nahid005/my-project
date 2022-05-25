fd2e30fd7a82d0e1a60d2eca28eac1174fb0e6d1
Nahid005 <nahidhassan9258@gmail.com>
2022-05-25 08:25:10 +0600

    clock rendaring

--- src/component/Button.js
+++ src/component/Button.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 class Button extends React.Component {
 
-    shouldComponentUpdate(nextprops) {
-        const {change:carrentProps} = this.props;
-        const {change: nextProps} = nextprops;
+    // shouldComponentUpdate(nextprops) {
+    //     const {change:carrentProps} = this.props;
+    //     const {change: nextProps} = nextprops;
 
-        if(carrentProps === nextProps){
-            return false;
-        }else{
-            return true
-        }
-    }
+    //     if(carrentProps === nextProps){
+    //         return false;
+    //     }else{
+    //         return true
+    //     }
+    // }
 
 
     render() {
-        let {change} = this.props;
+        let {change, local} = this.props;
 
         // console.log('button component ')
         
         return(
             <div>
-                <button type='button' onClick={change}> click here </button>
+                <button type='button' onClick={() => change(local)} > click here </button>
             </div>
         )
     }
